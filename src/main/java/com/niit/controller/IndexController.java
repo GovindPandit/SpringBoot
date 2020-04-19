7c5345427739b7836cc6a060f26c7cdf759c9d85
Niit1 <Niit1@DESKTOP-QQU94QK>
2020-04-19 18:34:49 +0530

    first spring boot commit

---
+++ src/main/java/com/niit/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.niit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController
+{
+	@RequestMapping(value = {"","/","/home"})
+	@ResponseBody
+	public String m1()
+	{
+		return "index!!!";
+	}
+}
